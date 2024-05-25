093cb5f5dc2350218186f3a29f0ed81432047a14
tkaseke <tkaseke@stumail.jccc.edu>
2024-05-24 19:30:23 -0500

    Create Django specific app page

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -20,7 +20,15 @@ Including another URLconf
 #from django.contrib import admin
 #from django.urls import path
 
-urlpatterns = [
+#urlpatterns = [
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
+#]
+from django.urls import include, re_path
+import MyApp1.views
+
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+    re_path(r'^$', MyApp1.views.index, name='index'),
+    re_path(r'^home$', MyApp1.views.index, name='home')
 ]
